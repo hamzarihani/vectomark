9b47344073c0b2a8ea95bcbc0e53b01703c18eda
hamzarihani <hamzarihani66@gmail.com>
2026-08-18 22:42:24 +0100

    add reusable ui components and utilities

---
+++ src/components/ui/backgrounds.tsx
@@ -0,0 +1,7 @@
+export const DotGrid = () => (
+  <div className="fixed inset-0 -z-10 h-full w-full bg-[radial-gradient(#262626_1px,transparent_1px)] [background-size:16px_16px] opacity-40 pointer-events-none"></div>
+);
+
+export const RadialGlow = () => (
+  <div className="fixed top-0 left-1/2 -z-10 h-[800px] w-[800px] -translate-x-1/2 -translate-y-1/2 opacity-20 blur-[100px] bg-primary rounded-full pointer-events-none"></div>
+);
